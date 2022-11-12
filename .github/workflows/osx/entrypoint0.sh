3885b9ed48415fd140f69cf6f4c47ae16d503836
Alois Kabelschacht <andrii.verbytskyi@mpp.mpg.de>
2022-11-12 20:49:03 +0100

    OK

--- .github/workflows/osx/entrypoint0.sh
+++ .github/workflows/osx/entrypoint0.sh
@@ -15,6 +15,7 @@ brew install automake
 brew install libtool 
 brew install pkg-config
 
+
    export CXX=g++-12
    export CC=gcc-12
    export FC=gfortran-12
@@ -22,12 +23,14 @@ brew install pkg-config
 git clone https://gitlab.cern.ch/averbyts/sherpa
 cd sherpa
 git checkout cmake
+
+
 cmake -S . -B BUILD -DCMAKE_INSTALL_PREFIX=$(pwd)/CM \
           -DCMAKE_CXX_FLAGS="-O2 -g -fcx-fortran-rules" -DCMAKE_C_FLAGS="-O2 -g" -DCMAKE_Fortran_FLAGS="-O2 -g" \
           -DLHAPDF_DIR=/usr/local/Cellar/lhapdf/6.5.3  \
           -DSHERPA_ENABLE_MCFM=OFF  \
           -DSHERPA_ENABLE_HEPMC3_ROOT=OFF  \
-          -DSHERPA_ENABLE_MANUAL=ON  \
+          -DSHERPA_ENABLE_MANUAL=OFF  \
           -DHepMC2_DIR=/usr/local/Cellar/hepmc2/2.06.11 \
            -DSHERPA_ENABLE_RECOLA=OFF \
            -DSHERPA_ENABLE_HZTOOL=OFF \
