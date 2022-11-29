896e1f7c79a9a268eebdc0865d7bf5bc029fc7cf
andriish <andriish@users.noreply.github.com>
2022-11-29 11:17:32 +0100

    Update entrypoint0.sh

--- .github/workflows/osx/entrypoint0.sh
+++ .github/workflows/osx/entrypoint0.sh
@@ -16,21 +16,19 @@ brew install libtool
 brew install pkg-config
 
 
- #  export CXX=g++-12
- #  export CC=gcc-12
-   export FC=gfortran-12
+export FC=gfortran-12
 
-git clone --depth 1 https://gitlab.cern.ch/averbyts/sherpa -b cmake
+git clone --depth 1 https://gitlab.com/andriish/sherpa -b cmake
 cd sherpa
 #-fcx-fortran-rules
 
 cmake -S . -B BUILD -DCMAKE_INSTALL_PREFIX=$(pwd)/CM \
-          -DCMAKE_CXX_FLAGS="-O2 -g " -DCMAKE_C_FLAGS="-O2 -g" -DCMAKE_Fortran_FLAGS="-O2 -g" \
-          -DLHAPDF_DIR=/usr/local/Cellar/lhapdf/6.5.3  \
-          -DSHERPA_ENABLE_MCFM=OFF  \
-          -DSHERPA_ENABLE_HEPMC3_ROOT=OFF  \
-          -DSHERPA_ENABLE_MANUAL=OFF  \
-          -DHepMC2_DIR=/usr/local/Cellar/hepmc2/2.06.11 \
+           -DCMAKE_CXX_FLAGS="-O2 -g " -DCMAKE_C_FLAGS="-O2 -g" -DCMAKE_Fortran_FLAGS="-O2 -g" \
+           -DLHAPDF_DIR=/usr/local/Cellar/lhapdf/6.5.3  \
+           -DSHERPA_ENABLE_MCFM=OFF  \
+           -DSHERPA_ENABLE_HEPMC3_ROOT=OFF  \
+           -DSHERPA_ENABLE_MANUAL=OFF  \
+           -DHepMC2_DIR=/usr/local/Cellar/hepmc2/2.06.11 \
            -DSHERPA_ENABLE_RECOLA=OFF \
            -DSHERPA_ENABLE_HZTOOL=OFF \
            -DSHERPA_ENABLE_ROOT=OFF \
@@ -38,11 +36,15 @@ cmake -S . -B BUILD -DCMAKE_INSTALL_PREFIX=$(pwd)/CM \
            -DSHERPA_ENABLE_RIVET=OFF \
            -DSHERPA_ENABLE_GOSAM=OFF \
            -DSHERPA_ENABLE_MADLOOP=OFF \
-           -DCMAKE_CXX_STANDARD=11 \
            -DSHERPA_ENABLE_PYTHIA8=OFF \
            -DSHERPA_ENABLE_OPENLOOPS=OFF \
+           -DSHERPA_ENABLE_EWSUD=ON \
+           -DSHERPA_ENABLE_TESTING=ON \
           
 cmake --build BUILD -j 2
 cmake --install BUILD
+ctest --test-dir BUILD
+lhapdf install PDF4LHC21_40_pdfas
+CM/bin/Sherpa Examples/CI/LO_Z/Sherpa.yaml
 
 
